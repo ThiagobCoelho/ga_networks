14d4493e088af2cae8f6b6eb7c9aa501137e2e20
Thiago Coelho <tbarroscoelho@gmail.com>
2017-11-02 13:59:50 -0300

    First commit, adding files

---
+++ do.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+
+(make && make && make && make && make && make && make && make) & # 8
+sleep 20
+(make && make && make && make && make && make && make && make) & # 16
+sleep 20 
+(make && make && make && make && make && make && make && make) & # 24
